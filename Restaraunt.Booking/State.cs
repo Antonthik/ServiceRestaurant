31f8857486aa9e4a89d7c44993b185424b222db4
antonchik <ganseanderson@gmail.com>
2022-10-20 22:22:39 +0300

    les2 Реализация брокера сообщений на базе облачного RabbitMQ.Нстройка брокера. Реализация клиентского сервиса сообщений на хосте.Передача и прием сообщений между разными приложениями.

---
+++ Restaraunt.Booking/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaraunt.Booking
+{
+
+    public enum State
+    {
+        /// <summary>
+        /// Стол свободен
+        /// </summary>
+        Free = 0,
+
+        /// <summary>
+        /// Стол занят
+        /// </summary>
+        Booked = 1
+    }
+}
